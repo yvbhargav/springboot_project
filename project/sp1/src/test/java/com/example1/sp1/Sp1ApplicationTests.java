b3a554d03ea4812eaf6eb737ac4cf9e73b6eab76
Venkata Yamakoti (WIPRO LIMITED) <v-vyamakoti@microsoft.com>
2025-07-29 19:14:14 +0530

    pushed

---
+++ project/sp1/src/test/java/com/example1/sp1/Sp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example1.sp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
