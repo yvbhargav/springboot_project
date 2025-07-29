c8501b00e6fff2af86555cb3c4eef97ae998dd4b
Venkata Yamakoti (WIPRO LIMITED) <v-vyamakoti@microsoft.com>
2025-07-29 22:48:48 +0530

    Added sp3 as a subfolder/module

---
+++ project/sp3/src/main/java/com/example3/sp3/Sp3Application.java
@@ -0,0 +1,13 @@
+package com.example3.sp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp3Application.class, args);
+	}
+
+}
